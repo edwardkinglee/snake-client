8bef67220183e9923df6980fd8bfa0f491c178d1
edwardkinglee <edward.lee3@telus.com>
2022-11-23 18:46:11 -0500

    created input.js file

--- client.js
+++ client.js
@@ -1,6 +1,6 @@
 const net = require("net");
 // establishes a connection with the game server
-const connect = function () {
+const connect = function() {
   const conn = net.createConnection({
     host: "165.227.47.243",
     port: 50541,
---
+++ input.js
@@ -0,0 +1,19 @@
+const handleUserInput = function() {
+  // your code here
+};
+
+// setup interface to handle user input from stdin
+
+const setupInput = function() {
+  const stdin = process.stdin;
+  stdin.setRawMode(true);
+  stdin.setEncoding("utf8");
+  stdin.resume();
+  stdin.on("data", handleUserInput);
+  return stdin;
+};
+
+module.exports = {
+  handleUserInput,
+  setupInput
+};
