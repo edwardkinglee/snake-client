308758cf4febc9932f8988af6b58dade6503d8fa
edwardkinglee <edward.lee3@telus.com>
2022-11-23 18:01:28 -0500

    tested up down left right commands

--- client.js
+++ client.js
@@ -1,6 +1,6 @@
 const net = require("net");
 // establishes a connection with the game server
-const connect = function() {
+const connect = function () {
   const conn = net.createConnection({
     host: "165.227.47.243",
     port: 50541,
@@ -13,6 +13,10 @@ const connect = function() {
     // code that does something when the connection is first established
     console.log("Successfully connected to game server");
     conn.write("Name: EL");
+    // setInterval(() => conn.write("Move: down"), 50);
+    // setTimeout(() => conn.write("Move: left"), 50);
+    // setTimeout(() => conn.write("Move: left"), 50);
+    // setTimeout(() => conn.write("Move: left"), 50);
   });
   // gets data from server
   conn.on('data', (data) => {
