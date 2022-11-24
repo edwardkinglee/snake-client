5cf69b6997a01941b976beb7ad2a2bcd46d04fcf
edwardkinglee <edward.lee3@telus.com>
2022-11-23 20:10:13 -0500

    added constants

--- input.js
+++ input.js
@@ -1,20 +1,21 @@
+const { MOVE_UP_KEY, MOVE_DOWN_KEY, MOVE_LEFT_KEY, MOVE_RIGHT_KEY } = require("./constants");
 let connection;
 
 const handleUserInput = function(key) {
   // your code here
   
-  if (key === 'w') {
+  if (key === MOVE_UP_KEY) {
     connection.write('Move: up');
   }
 
-  if (key === 'a') {
+  if (key === MOVE_LEFT_KEY) {
     connection.write('Move: left');
   }
 
-  if (key === 's') {
+  if (key === MOVE_DOWN_KEY) {
     connection.write('Move: down');
   }
-  if (key === 'd') {
+  if (key === MOVE_RIGHT_KEY) {
     connection.write('Move: right');
   }
 };
